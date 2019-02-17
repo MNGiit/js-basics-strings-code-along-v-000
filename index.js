d691b300011e6283d7dd4cbfb2f950a5aa8cf871
Mohammed Noori <kmanoorib@gmail.com>
2019-02-17 04:25:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ let welcomeMessage = "Welcome to Flatbook, !";
 
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, !";
 
-let shortGreeting = "Welcome, ";
+let shortGreeting = "Welcome, " + currentUser;
 
 
