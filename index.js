a50463d896aa7cc25441fd8551129f8030ac5b01
Mohammed Noori <kmanoorib@gmail.com>
2019-02-17 04:30:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let currentUser = "Edsger Dijkstra";
 
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
 
-let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase + "!";
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
 
 let firstInitial = currentUser[0];
 
